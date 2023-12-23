8927c8f5591540ff4f1281bd0a1993b15b947eb9
Fahad <205fahadmohammad@gmail.com>
2023-12-23 20:33:25 +0600

    [done] signup authentication

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,32 +1,18 @@
-import { NextFunction, Request, Response } from 'express'
-import httpStatus from 'http-status'
-import { Secret } from 'jsonwebtoken'
-import ApiError from '../../../errors/ApiError'
-import { jwtHelpers } from '../../../helpers/jwtHelpers'
-import config from '../../../config'
+import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
+import { AuthController } from './auth.controller'
+import { AuthValidation } from './auth.validation'
 
-const auth =
-  (...requiredRoles: string[]) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const token = req.headers.authorization
-      if (!token) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized')
-      }
+const router = express.Router()
 
-      let verifiedUser = null
+router.post(
+  '/signup',
+  validateRequest(AuthValidation.create),
+  AuthController.createUser,
+)
 
-      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret)
+router.post('/signin', AuthController.loginUser)
 
-      req.user = verifiedUser
+router.post('/reset-token', AuthController.resetToken)
 
-      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
-        throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden Access')
-      }
-      next()
-    } catch (error) {
-      next(error)
-    }
-  }
-
-export default auth
+export const AuthRoutes = router
---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+const create = z.object({
+  body: z.object({
+    firstName: z.string({
+      required_error: 'FirstName is required',
+    }),
+    lastName: z.string({
+      required_error: 'LastName is required',
+    }),
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+  }),
+})
+
+export const AuthValidation = {
+  create,
+}
