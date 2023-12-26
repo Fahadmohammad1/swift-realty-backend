bf9e6a343b44101b11cf80ffb825e6ad90ce699e
Fahad <205fahadmohammad@gmail.com>
2023-12-26 20:51:55 +0600

    [done] add property with validation

--- src/app/modules/property/property.routes.ts
+++ src/app/modules/property/property.routes.ts
@@ -1,16 +1,16 @@
 import express from 'express'
-// import validateRequest from '../../middlewares/validateRequest'
+import validateRequest from '../../middlewares/validateRequest'
 import { PropertyController } from './property.controller'
 import auth from '../../middlewares/auth'
 import { ENUM_USER_ROLE } from '../../../enums/user'
-// import { PropertyValidation } from './property.validation'
+import { PropertyValidation } from './property.validation'
 
 const router = express.Router()
 
 router.post(
   '/add',
   auth(ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.ADMIN),
-  // validateRequest(PropertyValidation.add),
+  validateRequest(PropertyValidation.add),
   PropertyController.addProperty,
 )
 
--- src/app/modules/property/property.validation.ts
+++ src/app/modules/property/property.validation.ts
@@ -17,6 +17,42 @@ const add = z.object({
     category: z.string({
       required_error: 'category is required',
     }),
+    status: z.string({
+      required_error: 'status is required',
+    }),
+    bed: z.string({
+      required_error: 'bed is required',
+    }),
+    baths: z.string({
+      required_error: 'baths is required',
+    }),
+    ac: z.string({
+      required_error: 'ac is required',
+    }),
+    gym: z.string({
+      required_error: 'gym is required',
+    }),
+    swimmingPool: z.string({
+      required_error: 'swimmingPool is required',
+    }),
+    wifi: z.string({
+      required_error: 'wifi is required',
+    }),
+    washer: z.string({
+      required_error: 'washer is required',
+    }),
+    garage: z.string({
+      required_error: 'garage is required',
+    }),
+    fireplace: z.string({
+      required_error: 'fireplace is required',
+    }),
+    availablity: z.string({
+      required_error: 'availablity is required',
+    }),
+    type: z.string({
+      required_error: 'type is required',
+    }),
   }),
 })
 
